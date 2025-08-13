390fc95980d6d920094b767fe39c127423970c3f
august <me@augusttippett.com>
2025-08-12 18:52:56 -0700

    Got file io working just a basic int!!!

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ mod parser;
 mod executor;
 mod types;
 
-fn main() {
-    storage::read_int_from_file();
-    storage::write_int_to_file();
+fn main() -> std::io::Result<()> {
+    storage::write_int_to_file("example.txt", 1)?;
+    let num = storage::read_int_from_file("example.txt").unwrap();
+    println!("We just read this from a file: {:?}!!!", num);
+    Ok(())
 }
--- src/storage.rs
+++ src/storage.rs
@@ -1,10 +1,18 @@
-use std::fs::{File, OpenOptions};
+use std::fs::{File};
 use std::io::{self, Read, Write}; 
 
-pub fn write_int_to_file() ->  {
-
+pub fn write_int_to_file(path: &str, content: i32) -> io::Result<()> {
+    let mut file = File::create(path)?;
+    file.write_all(&content.to_le_bytes())?;
+    Ok(())
 } 
 
-pub fn read_int_from_file() ->  {
-
+pub fn read_int_from_file(path: &str) -> io::Result<i32> {
+    let mut file = File::open(path)?;
+     let mut buffer = [0u8; 4];
+    file.read_exact(&mut buffer)?;
+   
+    let content= i32::from_le_bytes(buffer);
+    
+    Ok( content )
 }
